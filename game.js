7392c99b393f80726b32e2d3ea47956884ebac40
Bill Klymus <bktx75@gmail.com>
2022-06-17 13:06:46 -0500

    add game function to set loop and alert user of final score

--- game.js
+++ game.js
@@ -9,26 +9,51 @@ function computerPlay() {
 //  Plays a round between computer and user
 function round(playerSelection, computerSelection) { 
     playerSelection = playerSelection.toLowerCase();
-    console.log(computerSelection);
+    
     if (playerSelection === 'paper' && computerSelection === 'rock') {
         alert("Paper wraps rock, you win!");
+        playerWin ++;
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
         alert("Rock blunts scissors, you win!");
+        playerWin ++;
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
         alert("Scissors cut paper - you win!");
+        playerWin ++;
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
         alert("Scissors cut paper - I win!");
+        computerWin ++;
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         alert("Paper wraps rock - I win!");
+        computerWin ++;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
         alert("Rock blunts scissors - I win!");
+        computerWin++;
     } else {
         alert("It's a tie!");
+        tie++;
     }
-  
+
 }
 
-round(prompt("paper, rock, scissors?"), computerPlay());
+// Sets a loop to play five games
+function game() {
+    
+    for (let i = 0; i<5; i++){
+        round(prompt("Paper, rock or scissors?"), computerPlay());
+    }
+    
+    alert(`You won ${playerWin}, I won ${computerWin}, we tied ${tie}.`);    
+}
+
+
+// Keeps track of score
+    let playerWin = 0;
+    let computerWin = 0;
+    let tie = 0;
+
+//Function call to start game
+game()
+
 
 
 
