f51c99e3e565ed7e9d063287b1e5884bdadef296
Bill Klymus <bktx75@gmail.com>
2022-06-17 12:21:41 -0500

    add if else statements

--- game.js
+++ game.js
@@ -9,10 +9,23 @@ function computerPlay() {
 //  Plays a round between computer and user
 function round(playerSelection, computerSelection) { 
     playerSelection = playerSelection.toLowerCase();
-    
-    if (playerSelection === 'paper' && computerSelection === 'rock') {alert("Paper wraps rock, you win!");}
-
-    
+    console.log(computerSelection);
+    if (playerSelection === 'paper' && computerSelection === 'rock') {
+        alert("Paper wraps rock, you win!");
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert("Rock blunts scissors, you win!");
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert("Scissors cut paper - you win!");
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert("Scissors cut paper - I win!");
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        alert("Paper wraps rock - I win!");
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert("Rock blunts scissors - I win!");
+    } else {
+        alert("It's a tie!");
+    }
+  
 }
 
 round(prompt("paper, rock, scissors?"), computerPlay());
