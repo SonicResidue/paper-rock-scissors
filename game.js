defa83080c5ee1a98b768dc5c884e9e4351ce14f
Bill Klymus <bktx75@gmail.com>
2022-06-16 20:10:37 -0500

    add functions to game

--- game.js
+++ game.js
@@ -1,16 +1,23 @@
 
-
-
-console.log(computerPlay());
-
-
-
-
-
-
+// Computer randomly selects paper, rock, or scissors
 function computerPlay() {
     let pick = ['rock', 'paper', 'scissors'];
     let random = pick[Math.floor(Math.random() * 3)];
-
     return random;
 }
+
+//  Plays a round between computer and user
+function round(playerSelection, computerSelection) { 
+    playerSelection = playerSelection.toLowerCase();
+    
+    if (playerSelection === 'paper' && computerSelection === 'rock') {alert("Paper wraps rock, you win!");}
+
+    
+}
+
+round(prompt("paper, rock, scissors?"), computerPlay());
+
+
+
+
+
